314b2314217b4d784928b39c5ab74604d9a57d68
Daniel Reis <pigmoth@gmail.com>
2018-03-01 19:16:59 +0000

    Distance between rides

--- src/Distance.java
+++ src/Distance.java
@@ -13,4 +13,9 @@ public class Distance {
     public static Integer getDistance(int[] pos1, int[] pos2) {
         return getDistance(pos1[0], pos2[0], pos1[1], pos2[1]);
     }
+
+    public static Integer getDistance(Ride r1,Ride r2) {
+        return getDistance(r1.getBeginLocal()[0], r2.getEndLocal()[0], r1.getBeginLocal()[1], r2.getEndLocal()[1]);
+    }
+
 }
