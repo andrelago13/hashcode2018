328d821971eb74931a56e556c4936f3439da98c9
André Lago <andrelago1995@gmail.com>
2018-03-01 17:42:16 +0000

    Template files

---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+public class Utils {
+    public static void solve() {
+        Object input = parse();
+        Object result = Solver.solve(input);
+        storeResult(result);
+    }
+
+    public static Object parse() {
+
+    }
+
+    public static void storeResult(Object result) {
+
+    }
+}
