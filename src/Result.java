ddc66efeafca1e16f6ac6f13660ab6c1415bd3b1
Daniel Reis <pigmoth@gmail.com>
2018-03-01 19:51:53 +0000

    Output

---
+++ src/Result.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Result {
+    private int nRides;
+    private ArrayList<Integer> rideNumbers;
+
+    public Result(int nRides, ArrayList<Integer> rideNumbers) {
+        this.nRides = nRides;
+        this.rideNumbers = rideNumbers;
+    }
+
+    public int getnRides() {
+        return nRides;
+    }
+
+    public ArrayList<Integer> getRideNumbers() {
+        return rideNumbers;
+    }
+}
